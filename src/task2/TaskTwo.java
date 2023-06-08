517fd781176132ea5c6624b7b63fd10b65ec6c63
Rohlend1 <gskosh1w@gmail.com>
2023-06-08 10:59:11 +0300

    task 1 and task 2

--- src/task2/TaskTwo.java
+++ src/task2/TaskTwo.java
@@ -3,17 +3,10 @@ package task2;
 import java.io.*;
 import java.util.concurrent.*;
 
-//2 задачка: Написать программу со следующим функционалом:
-//Создать 2 исполнителя(ExecutorService).
-// Первый - фабричная однопоточная реализация, второй - реализация ThreadPoolExecutor.
-// Также необходимо создать thread safe коллекцию, в которую 2 исполнителя будут записывать данные.
-//Также создать поток-демон, который будет вычитывать содержимое коллекции, если в нее что-то добавляется - выводить в консоль,
-// используя system.out(при вычитке - из коллекции элемент убирать).
-//2 исполнителя должны принять на выполнение задачу  типа Runnable на вычитку всего содержимого(несколько строк текста)
-// из текстового фаила(путь до фаила, имя фаила передавать через конструктор, коллекцию для записи). Коллекция для записи c типом String
 public class TaskTwo {
 
-    public static final String PATH = "src\\task2\\text.txt";
+    public static final String PATH_TO_FILE1 = "src\\task2\\text.txt";
+    public static final String PATH_TO_FILE2 = "src\\task2\\text1.txt";
     private static final ArrayBlockingQueue<String> map = new ArrayBlockingQueue<>(20);
 
 
@@ -35,14 +28,12 @@ public class TaskTwo {
         daemonThread.setDaemon(true);
         daemonThread.start();
 
-        singleExecutorService.execute(new RunnableImpl(PATH));
-        threadPoolExecutorService.submit(new RunnableImpl(PATH));
+        singleExecutorService.execute(new RunnableImpl(PATH_TO_FILE1));
+        threadPoolExecutorService.submit(new RunnableImpl(PATH_TO_FILE2));
 
         singleExecutorService.shutdown();
         threadPoolExecutorService.shutdown();
 
-        singleExecutorService.close();
-        threadPoolExecutorService.close();
         System.out.println(System.currentTimeMillis()-startTime);
     }
     static class RunnableImpl implements Runnable{
